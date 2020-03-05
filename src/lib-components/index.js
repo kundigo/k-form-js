2a3e18fdf8d289daf90cb2e2c1ccce3a06a0e2e2
Kundigo CI <dorian+ci@kundigo.pro>
2020-03-05 22:43:35 +0000

    new commit

--- src/lib-components/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as CheckBox } from './check_box.vue';
-export { default as Date } from './date.vue';
-export { default as Datetime } from './datetime.vue';
-export { default as Field } from './field.vue';
-export { default as Form } from './form.vue';
-export { default as Select } from './select.vue';
-export { default as Textarea } from './textarea.vue';
-export { default as FormStore } from './form_store.js';
